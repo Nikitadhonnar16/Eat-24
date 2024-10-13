bd60a8ea534972648278a6aa2e82964f0b8f86dc
NikitaDhonnar <nikita.d@evpe.in>
2024-10-14 00:11:35 +0530

    Updates

---
+++ Grocery.779e15f1.js
@@ -0,0 +1,3 @@
+var e=globalThis.parcelRequire1f46;(0,e.register)("iO0e0",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),t=()=>/*#__PURE__*/(0,i.jsx)("h1",{children:"Our Grocery online store, and we have a lot of child components inside this web page!!!"})});//# sourceMappingURL=Grocery.779e15f1.js.map
+
+//# sourceMappingURL=Grocery.779e15f1.js.map
